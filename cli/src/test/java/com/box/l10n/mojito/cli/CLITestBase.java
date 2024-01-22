85ecc82590568260efc03f816a37569cb1bae944
Hylston Barbosa <hylstonnb@gmail.com>
2024-01-22 18:36:53 -0300

    LocalServerPort import update

--- cli/src/test/java/com/box/l10n/mojito/cli/CLITestBase.java
+++ cli/src/test/java/com/box/l10n/mojito/cli/CLITestBase.java
@@ -34,7 +34,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.OutputCaptureRule;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
--- webapp/src/test/java/com/box/l10n/mojito/rest/WSTestBase.java
+++ webapp/src/test/java/com/box/l10n/mojito/rest/WSTestBase.java
@@ -20,7 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
