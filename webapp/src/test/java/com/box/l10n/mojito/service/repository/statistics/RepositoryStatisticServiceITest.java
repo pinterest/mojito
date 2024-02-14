3d1168085e4ed08a44b6cc4b7b986fafe69ccd64
Jean Aurambault <aurambaj@users.noreply.github.com>
2024-02-13 20:44:11 -0800

    WIP to profile

---
+++ webapp/src/test/java/com/box/l10n/mojito/service/repository/statistics/RepositoryStatisticServiceITest.java
@@ -0,0 +1,29 @@
+package com.box.l10n.mojito.service.repository.statistics;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import com.box.l10n.mojito.service.assetExtraction.ServiceTestBase;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author jaurambault
+ */
+public class RepositoryStatisticServiceITest extends ServiceTestBase {
+
+  /**
+   * logger
+   */
+  static Logger logger = getLogger(RepositoryStatisticServiceITest.class);
+
+  @Autowired
+  RepositoryStatisticService repositoryStatisticService;
+
+  @Test
+  public void updateStatistics() {
+    System.out.println("update statistic");
+    repositoryStatisticService.updateStatistics(50L);
+  }
+
+}
