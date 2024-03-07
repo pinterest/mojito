90b00544f1a441525b233a048c189284162f737f
Jean Aurambault <aurambaj@users.noreply.github.com>
2024-03-06 21:34:22 -0800

    fix spring session with hsql
    can't use spring.session.store-type=none
    try to use session with hsql too

--- webapp/src/main/java/com/box/l10n/mojito/entity/RepositoryStatistic.java
+++ webapp/src/main/java/com/box/l10n/mojito/entity/RepositoryStatistic.java
@@ -69,6 +69,7 @@ public class RepositoryStatistic extends AuditableEntity {
   @JsonManagedReference
   @OneToMany(mappedBy = "repositoryStatistic", fetch = FetchType.EAGER)
   // TODO(ja/lib) Caused by: org.hibernate.NotYetImplementedFor6Exception: Ordering for ToOneAttributeMapping(NavigableRole[com.box.l10n.mojito.entity.RepositoryLocaleStatistic.locale])@501185817 not supported
+  // is that fine now?
 //  @OrderBy(value = "locale")
 
 //  Association laziness now respected
