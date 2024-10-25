750e0ca92e7095151e16ba9940f98ed9436d86fe
Matt <mwilshire@pinterest.com>
2024-10-25 14:24:42 +0100

    Review comments to fix up SQL schema

--- webapp/src/main/resources/db/migration/V71__Add_scheduled_jobs.sql
+++ webapp/src/main/resources/db/migration/V71__Add_scheduled_jobs.sql
@@ -1,31 +1,31 @@
 CREATE TABLE scheduled_job (
-    id CHAR(36) PRIMARY KEY,
-    repository_id bigint NOT NULL,
-    job_type smallint DEFAULT NULL,
-    cron varchar(50) DEFAULT NULL,
-    properties longtext NOT NULL,
-    job_status smallint NOT NULL,
-    start_date timestamp NULL DEFAULT NULL,
-    end_date timestamp NULL DEFAULT NULL,
+    id CHAR(36) PRIMARY KEY NOT NULL,
+    repository_id BIGINT NOT NULL,
+    job_type SMALLINT DEFAULT NULL,
+    cron VARCHAR(50) DEFAULT NULL,
+    properties LONGTEXT NOT NULL,
+    job_status SMALLINT NOT NULL,
+    start_date TIMESTAMP NULL DEFAULT NULL,
+    end_date TIMESTAMP NULL DEFAULT NULL,
     enabled TINYINT DEFAULT 1
 );
 
 CREATE TABLE scheduled_job_type (
-    id smallint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL
+    id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE scheduled_job_status_type (
-    id smallint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL
+    id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE scheduled_job
     ADD UNIQUE KEY UK__REPOSITORY_ID__JOB_TYPE(repository_id, job_type);
 
-alter table scheduled_job
-    add constraint FK__SCHEDULED_JOB__IMPORT_REPOSITORY__ID
-        foreign key (repository_id) references repository (id);
+ALTER TABLE scheduled_job
+    ADD CONSTRAINT FK__SCHEDULED_JOB__IMPORT_REPOSITORY__ID
+        FOREIGN KEY (repository_id) REFERENCES repository (id);
 
 ALTER TABLE scheduled_job
     ADD CONSTRAINT FK__JOB_TYPE__JOB_TYPE_ID FOREIGN KEY (job_type) REFERENCES scheduled_job_type(id);
@@ -39,15 +39,15 @@ INSERT INTO scheduled_job_status_type(name) VALUES('SCHEDULED'), ('IN_PROGRESS')
 
 CREATE TABLE scheduled_job_aud (
     id CHAR(36) NOT NULL,
-    rev integer NOT NULL,
-    revtype tinyint,
-    revend integer,
-    repository_id bigint,
-    job_type smallint DEFAULT NULL,
-    cron varchar(50) DEFAULT NULL,
-    properties longtext,
-    job_status smallint,
-    start_date timestamp NULL DEFAULT NULL,
-    end_date timestamp NULL DEFAULT NULL,
+    rev INTEGER NOT NULL,
+    revtype TINYINT,
+    revend INTEGER,
+    repository_id BIGINT,
+    job_type SMALLINT DEFAULT NULL,
+    cron VARCHAR(50) DEFAULT NULL,
+    properties LONGTEXT,
+    job_status SMALLINT,
+    start_date TIMESTAMP NULL DEFAULT NULL,
+    end_date TIMESTAMP NULL DEFAULT NULL,
     enabled TINYINT DEFAULT 1
 );
