4b8f1b1668ddde03fbc7ed7314855f73ca0ac286
Darko <darkolopez@pinterest.com>
2024-11-26 18:16:59 -0400

    I18N-1323 Update Mojito CLI to use OpenAPI spec for rest calls Fixed pagination issue

--- cli/src/main/java/com/box/l10n/mojito/cli/command/UserCommand.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/UserCommand.java
@@ -5,6 +5,7 @@ import io.swagger.client.ApiException;
 import io.swagger.client.api.UserWsApi;
 import io.swagger.client.model.PageUser;
 import io.swagger.client.model.Pageable;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class UserCommand extends Command {
@@ -16,6 +17,7 @@ public abstract class UserCommand extends Command {
     Pageable pageable = new Pageable();
     pageable.setPage(0);
     pageable.setSize(Integer.MAX_VALUE);
+    pageable.setSort(List.of());
     return this.userClient.getUsers(pageable, username, null);
   }
 }
