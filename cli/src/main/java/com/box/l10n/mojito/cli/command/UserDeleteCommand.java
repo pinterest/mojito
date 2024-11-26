c160ac978347171cad764c62a8f9baaefe4329dc
Darko <darkolopez@pinterest.com>
2024-11-26 15:05:05 -0400

    I18N-1323 Update Mojito CLI to use OpenAPI spec for rest calls Refactored user rest calls

--- cli/src/main/java/com/box/l10n/mojito/cli/command/UserDeleteCommand.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/UserDeleteCommand.java
@@ -4,8 +4,13 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.box.l10n.mojito.cli.command.param.Param;
 import com.box.l10n.mojito.cli.console.ConsoleWriter;
-import com.box.l10n.mojito.rest.client.UserClient;
 import com.box.l10n.mojito.rest.client.exception.ResourceNotFoundException;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.UserWsApi;
+import io.swagger.client.model.PageUser;
+import io.swagger.client.model.Pageable;
+import io.swagger.client.model.User;
+import java.util.List;
 import org.fusesource.jansi.Ansi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -32,16 +37,25 @@ public class UserDeleteCommand extends Command {
       description = Param.USERNAME_DESCRIPTION)
   String username;
 
-  @Autowired UserClient userClient;
+  @Autowired UserWsApi userClient;
 
   @Override
   protected void execute() throws CommandException {
     consoleWriter.a("Delete user: ").fg(Ansi.Color.CYAN).a(username).println();
 
     try {
-      userClient.deleteUserByUsername(username);
+      Pageable pageable = new Pageable();
+      pageable.setPage(0);
+      pageable.setSize(Integer.MAX_VALUE);
+      PageUser pageUser = this.userClient.getUsers(pageable, username, null);
+      List<User> users = pageUser.getContent();
+      if (users.isEmpty()) {
+        throw new ResourceNotFoundException("User with username [" + username + "] is not found");
+      } else {
+        this.userClient.deleteUserByUserId(users.getFirst().getId());
+      }
       consoleWriter.newLine().a("deleted --> user: ").fg(Ansi.Color.MAGENTA).a(username).println();
-    } catch (ResourceNotFoundException ex) {
+    } catch (ResourceNotFoundException | ApiException ex) {
       throw new CommandException(ex.getMessage(), ex);
     }
   }
