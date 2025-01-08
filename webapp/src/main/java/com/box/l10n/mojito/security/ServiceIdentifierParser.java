863e144eada2390e6c9026be26892bb9ebc02e71
byronantak <bma.byron.antak@gmail.com>
2025-01-08 11:23:05 +0200

    Add Parsing functionality for service headers

---
+++ webapp/src/main/java/com/box/l10n/mojito/security/ServiceIdentifierParser.java
@@ -0,0 +1,23 @@
+package com.box.l10n.mojito.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceIdentifierParser {
+  private final ServiceKeyValueParser keyValueParser = new ServiceKeyValueParser();
+
+  @Autowired ServiceIdentifierParserConfig serviceIdentifierParserConfig;
+
+  String parseHeader(String header) throws ServiceNotIdentifiableException {
+    if (serviceIdentifierParserConfig.isEnabled()) {
+      return keyValueParser.parseHeader(
+          header,
+          serviceIdentifierParserConfig.getKeyValueDelimiter(),
+          serviceIdentifierParserConfig.getValueDelimiter(),
+          serviceIdentifierParserConfig.getIdentifierKey());
+    }
+
+    return header;
+  }
+}
