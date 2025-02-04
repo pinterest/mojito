a61b598b9be903f038c960c635e6f56fc8bbea6d
Matt <mwilshire@pinterest.com>
2025-02-04 18:03:09 +0000

    Only allow admins and PMs to view & trigger jobs

--- webapp/src/main/resources/public/js/components/jobs/JobsView.jsx
+++ webapp/src/main/resources/public/js/components/jobs/JobsView.jsx
@@ -3,6 +3,8 @@ import {withAppConfig} from "../../utils/AppConfig";
 import JobRow from "./JobRow";
 import JobStore from "../../stores/jobs/JobStore";
 import JobActions from "../../actions/jobs/JobActions";
+import AuthorityService from "../../utils/AuthorityService";
+import {FormattedMessage} from "react-intl";
 
 class JobsView extends React.Component {
 
@@ -13,11 +15,14 @@ class JobsView extends React.Component {
 
         // Bind this to the function call to ensure this references this instance.
         this.jobStoreChange = this.jobStoreChange.bind(this);
-        JobActions.getAllJobs();
 
-        this.interval = setInterval(() => {
+        if(AuthorityService.canViewJobs()) {
+            // Only poll if we have access, stops polling for 403 responses
             JobActions.getAllJobs();
-        }, 5000);
+            this.interval = setInterval(() => {
+                JobActions.getAllJobs();
+            }, 5000);
+        }
     }
 
     componentDidMount() {
@@ -40,6 +45,14 @@ class JobsView extends React.Component {
      * @return {XML}
      */
     render() {
+        if (!AuthorityService.canViewJobs()) {
+            return (
+                <div className="ptl">
+                    <h3 className="text-center mtl">You do not have permissions to view Jobs.</h3>
+                </div>
+            )
+        }
+
         return (
             <div>
                 <div className="jobs-container">
