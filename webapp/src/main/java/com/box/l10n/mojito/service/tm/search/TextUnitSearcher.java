9a4ba68f2b2522f7bc14feb4361ad15fa65a6786
Darko <darkolopez@pinterest.com>
2025-02-13 22:03:54 -0400

    I18N-1397 - Use branch id for text unit searcher on getTextUnitDTOSForTmTextUnitIds Modified text unit search query to get text units by branch ID using Branch Statistic table

--- webapp/src/main/java/com/box/l10n/mojito/service/tm/search/TextUnitSearcher.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/tm/search/TextUnitSearcher.java
@@ -150,8 +150,11 @@ public class TextUnitSearcher {
   }
 
   NativeCriteria getCriteriaForSearch(TextUnitSearcherParameters searchParameters) {
-
     Preconditions.checkNotNull(searchParameters, "Search parameters should not be null");
+    Preconditions.checkArgument(
+        searchParameters.getBranchId() == null
+            || searchParameters.getBranchStatisticBranchId() == null,
+        "Only one of branchId or branchStatisticBranchId should be provided, but both were given");
 
     logger.debug("Creating the native criteria with joins");
 
@@ -217,6 +220,22 @@ public class TextUnitSearcher {
     c.addJoin(
         new NativeJoin(
             "plural_form_for_locale", "pffl", NativeJoin.JoinType.LEFT_OUTER, onClausePluralForm));
+
+    if (searchParameters.getBranchStatisticBranchId() != null) {
+      NativeJunctionExp onClauseBranchTextUnitStatistic = NativeExps.conjunction();
+      onClauseBranchTextUnitStatistic.add(new NativeColumnEqExp("btus.tm_text_unit_id", "tu.id"));
+      c.addJoin(
+          new NativeJoin(
+              "branch_text_unit_statistic",
+              "btus",
+              NativeJoin.JoinType.LEFT_OUTER,
+              onClauseBranchTextUnitStatistic));
+      NativeJunctionExp onClauseBranchStatistic = NativeExps.conjunction();
+      onClauseBranchStatistic.add(new NativeColumnEqExp("bs.id", "btus.branch_statistic_id"));
+      c.addJoin(
+          new NativeJoin(
+              "branch_statistic", "bs", NativeJoin.JoinType.LEFT_OUTER, onClauseBranchStatistic));
+    }
 
     logger.debug("Set projections");
     NativeProjection projection =
@@ -487,6 +506,11 @@ public class TextUnitSearcher {
     if (searchParameters.getTmTextUnitCreatedAfter() != null) {
       conjunction.add(
           new NativeDateGteExp("tu.created_date", searchParameters.getTmTextUnitCreatedAfter()));
+    }
+
+    if (searchParameters.getBranchStatisticBranchId() != null) {
+      conjunction.add(
+          new NativeEqExpFix("bs.branch_id", searchParameters.getBranchStatisticBranchId()));
     }
 
     if (!conjunction.toSQL().isEmpty()) {
