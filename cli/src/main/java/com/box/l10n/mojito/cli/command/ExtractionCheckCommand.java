5e8b6c755a4bf18eda2f812b68b26aa3952e3819
bantak <bma.byron.antak@gmail.com>
2025-03-19 13:44:59 +0000

    Spotless formatting

--- cli/src/main/java/com/box/l10n/mojito/cli/command/ExtractionCheckCommand.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/ExtractionCheckCommand.java
@@ -28,7 +28,6 @@ import com.box.l10n.mojito.github.GithubClients;
 import com.box.l10n.mojito.github.GithubException;
 import com.box.l10n.mojito.okapi.extractor.AssetExtractorTextUnit;
 import com.box.l10n.mojito.regex.PlaceholderRegularExpressions;
-import com.box.l10n.mojito.resttemplate.AuthenticatedRestTemplate;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
