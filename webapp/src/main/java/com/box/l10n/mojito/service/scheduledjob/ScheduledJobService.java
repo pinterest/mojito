f4a16784f2ed6990564cefbf5970c3c902a3d69d
gerry004 <gerry04y@gmail.com>
2025-06-06 15:04:26 +0100

    bean annotation

--- webapp/src/main/java/com/box/l10n/mojito/service/scheduledjob/ScheduledJobService.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/scheduledjob/ScheduledJobService.java
@@ -8,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@Service
 public class ScheduledJobService {
 
   static Logger logger = LoggerFactory.getLogger(ScheduledJobService.class);
