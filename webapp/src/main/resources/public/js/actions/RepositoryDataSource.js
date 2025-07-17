cbd458e688f7a5d49cd1d33454ff0476d0991d2d
Gerry Yang <gerry04y@gmail.com>
2025-07-17 14:06:33 +0100

    Create Repository Modal UI (#326)
    * create repository modal
    
    * repository locales input ui
    
    * asset integrity checker input
    
    * repository locales input
    
    * rogue log
    
    * spotless
    
    * trailing whitespace error
    
    * remove unneccessary
    
    * transactional annotation error
    
    * repository locales input parent child
    
    * trailing whitespace
    
    * review comments

--- webapp/src/main/resources/public/js/actions/RepositoryDataSource.js
+++ webapp/src/main/resources/public/js/actions/RepositoryDataSource.js
@@ -2,6 +2,15 @@ import RepositoryClient from "../sdk/RepositoryClient.js";
 import RepositoryActions from "./RepositoryActions.js";
 
 const RepositoryDataSource = {
+    createRepository: {
+        remote(state, repository) {
+            return RepositoryClient.createRepository(repository);
+        },
+
+        success: RepositoryActions.createRepositorySuccess,
+        error: RepositoryActions.createRepositoryError
+    },
+
     getAllRepositories: {
         remote() {
             return RepositoryClient.getRepositories();
