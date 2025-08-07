32bbaf6e25d0c2ea3c45a142212b26070277d18f
Matt <43931909+mattwilshire@users.noreply.github.com>
2025-08-07 11:22:05 +0100

    Add SlidingWindowRateLimiter

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/ratelimiter/SlidingWindowRateLimiter.java
@@ -0,0 +1,42 @@
+package com.box.l10n.mojito.service.ratelimiter;
+
+import com.box.l10n.mojito.service.blobstorage.redis.RedisClient;
+import com.box.l10n.mojito.service.blobstorage.redis.RedisScript;
+import java.util.List;
+import redis.clients.jedis.exceptions.JedisException;
+
+public class SlidingWindowRateLimiter {
+  private final RedisClient redisClient;
+  private final String rateLimiterKey;
+  private final int maxRequests;
+  private final int windowSizeInMillis;
+
+  public SlidingWindowRateLimiter(
+      RedisClient redisClient, String rateLimiterKey, int maxRequests, int windowSizeInMillis) {
+    this.redisClient = redisClient;
+    this.rateLimiterKey = rateLimiterKey;
+    this.maxRequests = maxRequests;
+    this.windowSizeInMillis = windowSizeInMillis;
+  }
+
+  /**
+   * Checks if the request is allowed based on the sliding window rate limiting algorithm.
+   *
+   * @return true if the request is allowed, false if the rate limit has been exceeded
+   * @throws JedisException if there is an error executing the Redis script
+   */
+  public boolean isAllowed() throws JedisException {
+    // If Redis is not configured, allow all requests
+    if (redisClient != null) {
+      return (Long)
+              redisClient.executeScript(
+                  RedisScript.SLIDING_WINDOW_RATE_LIMITER,
+                  List.of(rateLimiterKey),
+                  List.of(String.valueOf(maxRequests), String.valueOf(windowSizeInMillis)))
+          == 1L;
+    } else {
+      // No rate limiting if Redis is not configured
+      return true;
+    }
+  }
+}
