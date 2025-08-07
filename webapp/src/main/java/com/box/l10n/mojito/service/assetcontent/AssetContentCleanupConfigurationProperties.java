7f22ff10de7366b407deb01d843269bfdd12e1f7
DarKhaos <40587669+DarKhaos@users.noreply.github.com>
2025-08-07 13:01:54 -0400

    I18N-1602 Increase in Pinboard/Webapp syncs causes pull run variant table size to increase (#348)
    * I18N-1602 Increase in Pinboard/Webapp syncs causes pull run variant table size to increase
    Implemented the Asset Content cleanup job
    
    * Fixed cycle issue
    
    * Changed SQL query to get stale asset contents
    
    * Fix query and improve unit tests
    
    * Modified AssetContentCleanupJob
    
    * Delete only Asset Contents
    
    * Enhance asset content deletion logic to consider deleted branches
    
    * Add maxNumberOfIterations property and update cleanup logic to limit iterations
    Add maxNumberOfIterations property and update cleanup logic to limit iterations
    
    * Renamed maxNumberOfIterations to maxNumberOfBatches
    
    * Refactor SQL queries in AssetExtractionRepository and AssetContentRepository to support HSQLDB
    
    * Fix cleanAssetContentData unit tests
    
    * Fix cleanAssetContentData unit tests
    
    * Fix the SQL issue caused by using limit in a subquery
    
    * Add comments to clarify retention period logic in cleanAssetContentData tests
    Add comments to clarify retention period logic in cleanAssetContentData tests

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetcontent/AssetContentCleanupConfigurationProperties.java
@@ -0,0 +1,49 @@
+package com.box.l10n.mojito.service.assetcontent;
+
+import java.time.Period;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("l10n.asset-content.cleanup-job")
+public class AssetContentCleanupConfigurationProperties {
+  private String cron = "0 0 0 * * ?";
+
+  private Period retentionPeriod = Period.ofMonths(6);
+
+  private int batchSize = 10000;
+
+  private int maxNumberOfBatches = 10;
+
+  public String getCron() {
+    return cron;
+  }
+
+  public void setCron(String cron) {
+    this.cron = cron;
+  }
+
+  public Period getRetentionPeriod() {
+    return retentionPeriod;
+  }
+
+  public void setRetentionPeriod(Period retentionPeriod) {
+    this.retentionPeriod = retentionPeriod;
+  }
+
+  public int getBatchSize() {
+    return batchSize;
+  }
+
+  public void setBatchSize(int batchSize) {
+    this.batchSize = batchSize;
+  }
+
+  public int getMaxNumberOfBatches() {
+    return maxNumberOfBatches;
+  }
+
+  public void setMaxNumberOfBatches(int maxNumberOfBatches) {
+    this.maxNumberOfBatches = maxNumberOfBatches;
+  }
+}
