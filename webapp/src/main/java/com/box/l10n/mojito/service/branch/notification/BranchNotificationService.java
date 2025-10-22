98e2ec9acbdcd93aa004dc58b498f633b0c99ebf
Darko <darkolopez@pinterest.com>
2025-10-22 15:03:18 -0400

    Test

--- webapp/src/main/java/com/box/l10n/mojito/service/branch/notification/BranchNotificationService.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/branch/notification/BranchNotificationService.java
@@ -41,6 +41,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Test
+ */
 @Component
 public class BranchNotificationService {
 
