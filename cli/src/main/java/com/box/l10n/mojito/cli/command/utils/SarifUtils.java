67caca997e9423bd161b8cc100cf1cd1d6d4db69
bantak <bma.byron.antak@gmail.com>
2025-11-12 11:52:28 +0000

    Remove mount path prefix for SARIF file validation

--- cli/src/main/java/com/box/l10n/mojito/cli/command/utils/SarifUtils.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/utils/SarifUtils.java
@@ -39,4 +39,24 @@ public class SarifUtils {
                   return merged;
                 }));
   }
+
+  public static Map<String, Set<Integer>> removeFilePathPrefix(
+      Map<String, Set<Integer>> fileToLineNumberMap, String prefixToRemove) {
+    if (prefixToRemove == null || prefixToRemove.isEmpty()) {
+      return fileToLineNumberMap;
+    }
+
+    return fileToLineNumberMap.entrySet().stream()
+        .collect(
+            Collectors.toMap(
+                entry -> {
+                  String filePath = entry.getKey();
+                  if (filePath.startsWith(prefixToRemove)) {
+                    return filePath.substring(prefixToRemove.length());
+                  } else {
+                    return filePath;
+                  }
+                },
+                Map.Entry::getValue));
+  }
 }
