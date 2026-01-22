13582ec85b27ff57f2d9e39e114be0f0ef909bd8
bantak <bma.byron.antak@gmail.com>
2026-01-22 15:41:21 +0100

    Create util hooks

---
+++ webapp/src/main/frontend-v2/src/hooks/useBranchName.ts
@@ -0,0 +1,8 @@
+import { useLocation } from "react-router";
+
+export function useBranchName(): string {
+    const location = useLocation();
+    return location.search
+        ? new URLSearchParams(location.search).get("branchName") || ""
+        : "";
+}
