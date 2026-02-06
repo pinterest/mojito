c4f579b6b327594aac78c71308dd11ac8f144ab3
bantak <bma.byron.antak@gmail.com>
2026-02-06 16:45:27 +0100

    Use react query mutation

--- webapp/src/main/frontend-v2/src/features/Branch/Screenshots/ScreenshotUploadModal.tsx
+++ webapp/src/main/frontend-v2/src/features/Branch/Screenshots/ScreenshotUploadModal.tsx
@@ -1,6 +1,7 @@
-import { Button, Modal } from "antd";
+import { Button, message, Modal } from "antd";
 import React, { memo, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import ScreenshotWizard from "./ScreenshotWizard";
 
@@ -18,73 +19,99 @@ interface BranchDetailsProps {
   onPreviewImage?: (screenshotUrl: string) => void;
 }
 
+interface ScreenshotUploadData {
+  files: File[];
+  screenshotToTextUnitMap: Map<string, number[]>;
+}
+
 const ScreenshotUpload: React.FC<BranchDetailsProps> = ({
   isOpen,
   setIsOpen,
   branchStats,
 }) => {
-  const { t } = useTranslation("branch");
-  const [isUploading, setIsUploading] = useState(false);
-  const dataToUpload = useRef<{
-    files: File[];
-    screenshotToTextUnitMap: Map<string, number[]>;
-  }>({ files: [], screenshotToTextUnitMap: new Map() });
+  const dataToUpload = useRef<ScreenshotUploadData>({
+    files: [],
+    screenshotToTextUnitMap: new Map(),
+  });
 
   const [canUpload, setCanUpload] = useState(false);
 
-  const handleOk = async () => {
-    setIsUploading(true);
-    const { files, screenshotToTextUnitMap } = dataToUpload.current;
+  const { t } = useTranslation("branch");
+  const queryClient = useQueryClient();
 
-    if (files.length === 0) {
-      setIsUploading(false);
+  const uploadScreenshotsMutation = useMutation({
+    mutationFn: async ({
+      files,
+      screenshotToTextUnitMap,
+    }: ScreenshotUploadData): Promise<void> => {
+      const imageUuidByFileName = new Map(
+        files.map((file) => [file.name, crypto.randomUUID()]),
+      );
+
+      const imageUploadPromises = await Promise.allSettled(
+        files.map(async (file) => {
+          const arrayBuffer = await file.arrayBuffer();
+          return uploadImage(imageUuidByFileName.get(file.name)!, arrayBuffer);
+        }),
+      );
+
+      const errorUploading = imageUploadPromises.some(
+        (result) => result.status === "rejected",
+      );
+
+      if (errorUploading) {
+        throw new Error("Error uploading images");
+      }
+
+      const screenshotRun: ScreenshotRun = {
+        id: branchStats.branch.repository.manualScreenshotRun.id,
+        name: undefined,
+        repository: undefined,
+        screenshots: Array.from(screenshotToTextUnitMap.entries()).map(
+          ([screenshotName, textUnitIds]) => {
+            return {
+              locale: branchStats.branch.repository.sourceLocale,
+              name: crypto.randomUUID(),
+              branch: {
+                id: branchStats.branch.id,
+              },
+              src: `api/images/${imageUuidByFileName.get(screenshotName)!}`,
+              textUnits: textUnitIds.map((id) => ({ tmTextUnit: { id } })),
+            };
+          },
+        ),
+      };
+
+      await uploadScreenshot(screenshotRun);
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: [
+          "branchTextUnitStatus",
+          {
+            branchName: branchStats.branch.name,
+            repoName: branchStats.branch.repository.name,
+          },
+        ],
+        exact: true,
+      });
+
+      await queryClient.invalidateQueries({
+        queryKey: ["branchStatistics", { branchName: branchStats.branch.name }],
+        exact: true,
+      });
+
       setIsOpen(false);
-      return;
-    }
+    },
+    onError: (error) => {
+      console.error("Error uploading screenshots", error);
+      message.error(t("uploadFailed"));
+    },
+  });
 
-    const imageUuidByFileName = new Map(
-      files.map((file) => [file.name, crypto.randomUUID()]),
-    );
-
-    const imageUploadPromises = await Promise.allSettled(
-      files.map(async (file) => {
-        const arrayBuffer = await file.arrayBuffer();
-        return uploadImage(imageUuidByFileName.get(file.name)!, arrayBuffer);
-      }),
-    );
-
-    const errorUploading = imageUploadPromises.some(
-      (result) => result.status === "rejected",
-    );
-
-    if (errorUploading) {
-      // TODO
-      setIsUploading(false);
-      return;
-    }
-
-    const screenshotRun: ScreenshotRun = {
-      id: branchStats.branch.repository.manualScreenshotRun.id,
-      name: undefined,
-      repository: undefined,
-      screenshots: Array.from(screenshotToTextUnitMap.entries()).map(
-        ([screenshotName, textUnitIds]) => {
-          return {
-            locale: branchStats.branch.repository.sourceLocale,
-            name: crypto.randomUUID(),
-            branch: {
-              id: branchStats.branch.id,
-            },
-            src: `api/images/${imageUuidByFileName.get(screenshotName)!}`,
-            textUnits: textUnitIds.map((id) => ({ tmTextUnit: { id } })),
-          };
-        },
-      ),
-    };
-
-    await uploadScreenshot(screenshotRun);
-    setIsUploading(false);
-    setIsOpen(false);
+  const handleOk = async () => {
+    const { files, screenshotToTextUnitMap } = dataToUpload.current;
+    uploadScreenshotsMutation.mutate({ files, screenshotToTextUnitMap });
   };
 
   const handleCancel = () => {
@@ -101,7 +128,7 @@ const ScreenshotUpload: React.FC<BranchDetailsProps> = ({
       title={t("uploadScreenshot")}
       open={isOpen}
       onCancel={handleCancel}
-      confirmLoading={isUploading}
+      confirmLoading={uploadScreenshotsMutation.status === "pending"}
       width='80vw'
       footer={[
         <Button key='back' onClick={handleCancel}>
@@ -109,6 +136,7 @@ const ScreenshotUpload: React.FC<BranchDetailsProps> = ({
         </Button>,
         <Button
           disabled={!canUpload}
+          loading={uploadScreenshotsMutation.status === "pending"}
           key='submit'
           type='primary'
           onClick={handleOk}
