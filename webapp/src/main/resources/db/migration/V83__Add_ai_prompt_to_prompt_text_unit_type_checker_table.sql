c2feefa61759c075dbeef655bd4ebeb5d8d9961f
Darko <darkolopez@pinterest.com>
2026-02-11 10:05:21 -0400

    Add 'on delete cascade' to foreign key constraint in ai_prompt_to_prompt_text_unit_type_checker table

--- webapp/src/main/resources/db/migration/V83__Add_ai_prompt_to_prompt_text_unit_type_checker_table.sql
+++ webapp/src/main/resources/db/migration/V83__Add_ai_prompt_to_prompt_text_unit_type_checker_table.sql
@@ -4,4 +4,4 @@ CREATE TABLE ai_prompt_to_prompt_text_unit_type_checker (
     primary key (ai_prompt_id)
 );
 
-ALTER TABLE ai_prompt_to_prompt_text_unit_type_checker ADD CONSTRAINT FK__AI_PROMPT_TO_TU_TYPE_CHECKER__AI_PROMPT__ID foreign key (ai_prompt_id) references ai_prompt (id);
+ALTER TABLE ai_prompt_to_prompt_text_unit_type_checker ADD CONSTRAINT FK__AI_PROMPT_TO_TU_TYPE_CHECKER__AI_PROMPT__ID foreign key (ai_prompt_id) references ai_prompt (id) on delete cascade;
