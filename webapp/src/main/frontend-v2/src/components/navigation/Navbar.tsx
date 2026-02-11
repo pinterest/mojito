66e7cbe6d64d9af533a73eb028a69234ea894a0c
bantak <bma.byron.antak@gmail.com>
2026-02-11 13:39:31 +0100

    Add navbar redirect to old app

--- webapp/src/main/frontend-v2/src/components/navigation/Navbar.tsx
+++ webapp/src/main/frontend-v2/src/components/navigation/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Menu, type MenuProps } from "antd";
-import { memo } from "react";
-import { useNavigate } from "react-router";
+import { memo, useMemo } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 import favicon from "@/assets/favicon.ico";
 
@@ -8,23 +8,35 @@ type MenuItem = Required<MenuProps>["items"][number];
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const selectedKeys = useMemo(() => {
+    if (location.pathname === "/branches") return ["branches"];
+  }, [location.pathname]);
 
   const menuItems: MenuItem[] = [
     {
       label: "Mojito",
       key: "home",
       icon: <img src={favicon} alt='favicon' width={16} height={16} />,
+      onClick: async () => {
+        // navigating to the older version of the app.
+        // we cannot use react router to navigate there because it's a different app
+        window.location.assign("/");
+      },
     },
     {
       label: "Branches",
       key: "branches",
-      onClick: () => {
-        navigate("/branches");
+      onClick: async () => {
+        await navigate("/branches");
       },
     },
   ];
 
-  return <Menu items={menuItems} mode='horizontal' />;
+  return (
+    <Menu selectedKeys={selectedKeys} items={menuItems} mode='horizontal' />
+  );
 };
 
 export default memo(Navbar);
