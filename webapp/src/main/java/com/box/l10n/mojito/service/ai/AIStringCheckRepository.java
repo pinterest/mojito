136fc73bb2f285ccb6da42511f7dac0094893c81
DarKhaos <40587669+DarKhaos@users.noreply.github.com>
2026-03-03 10:13:53 -0400

    I18N-1878 Implement a job to clean up the ai_string_check table (#430)
    * I18N-1878 Implement a job to clean up the ai_string_check table
    Implement AI String Check cleanup job and service with scheduled deletion of stale data
    
    * Added logging to the AI String Check cleanup job

--- webapp/src/main/java/com/box/l10n/mojito/service/ai/AIStringCheckRepository.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/ai/AIStringCheckRepository.java
@@ -1,8 +1,22 @@
 package com.box.l10n.mojito.service.ai;
 
 import com.box.l10n.mojito.entity.AIStringCheck;
+import java.time.ZonedDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource(exported = false)
-public interface AIStringCheckRepository extends JpaRepository<AIStringCheck, Long> {}
+public interface AIStringCheckRepository extends JpaRepository<AIStringCheck, Long> {
+  @Modifying
+  @Transactional
+  @Query(
+      """
+      delete from #{#entityName} aisc
+      where aisc.createdDate <= :beforeDate
+      """)
+  void deleteAllOlderThan(@Param("beforeDate") ZonedDateTime beforeDate);
+}
